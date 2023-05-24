241122917e92fdb31a0c3a46995dba55fc8f8d33
mingcheng <mingcheng@outlook.com>
2023-05-24 15:37:09 +0800

    replace ipinfo source to offical sdk

--- source/ipinfo.go
+++ source/ipinfo.go
@@ -2,34 +2,25 @@ package source
 
 import (
 	"context"
-	"fmt"
 	"github.com/mingcheng/simplyddns"
-	"github.com/tidwall/gjson"
 	"net"
+
+	"github.com/ipinfo/go/v2/ipinfo"
 )
 
 func init() {
 	const (
-		Name  = "ipinfo"
-		Token = "8fe8bfdbb0d459"
+		Name = "ipinfo"
 	)
 
 	fn := func(ctx context.Context, _ *simplyddns.SourceConfig) (*net.IP, error) {
-		data, err := RawStrByURL(context.Background(), "https://ipinfo.io", map[string]string{
-			"Accept":        "application/json",
-			"Authorization": fmt.Sprintf("Bearer %s", Token),
-		})
-
+		result, err := ipinfo.GetIPAddr()
 		if err != nil {
 			return nil, err
 		}
 
-		if result := gjson.Get(data, "ip").Str; result != "" {
-			ip := net.ParseIP(result)
-			return &ip, nil
-		}
-
-		return nil, fmt.Errorf("can not found address from %s", Name)
+		ip := net.ParseIP(result)
+		return &ip, nil
 	}
 
 	_ = simplyddns.RegisterSourceFunc(Name, fn)
