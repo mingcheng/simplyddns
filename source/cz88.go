3fa78d3c8e9ed25f2087c3da2601663ad1d7a9e6
mingcheng <mingcheng@apache.org>
2024-11-18 13:58:34 +0800

    add cz88 source ip supports

---
+++ source/cz88.go
@@ -0,0 +1,41 @@
+package source
+
+import (
+	"context"
+	"fmt"
+	"github.com/mingcheng/simplyddns"
+	"github.com/tidwall/gjson"
+	"net"
+	"net/http"
+)
+
+func init() {
+	const (
+		Name = "cz88"
+	)
+
+	fn := func(ctx context.Context, _ *simplyddns.SourceConfig) (*net.IP, error) {
+		data, err := RawStrByURL(context.Background(), `https://update.cz88.net/api/cz88/ip/geo?ip=`, map[string]string{
+			"Accept":     "application/json",
+			"User-Agent": UserAgentCurl,
+		})
+
+		if err != nil {
+			return nil, err
+		}
+
+		statusCode := gjson.Get(data, "code").Int()
+		if statusCode != http.StatusOK {
+			return nil, fmt.Errorf("status code is %d", statusCode)
+		}
+
+		if result := gjson.Get(data, "data.ip").Str; result != "" {
+			ip := net.ParseIP(result)
+			return &ip, nil
+		}
+
+		return nil, fmt.Errorf("can not found address from %s", Name)
+	}
+
+	_ = simplyddns.RegisterSourceFunc(Name, fn)
+}
